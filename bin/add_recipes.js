05494df5b74000a3a69020bd9aaae41aefda5ee7
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-09-13 16:44:47 -0400

    Exports the final client function, find_recipe, that returns a recipe that can be created with the ingredients given.

--- bin/add_recipes.js
+++ bin/add_recipes.js
@@ -20,7 +20,7 @@
     });
   }
 
-  recipes.get_first_recipe(['salt', 'condensed milk'], function(err, recipe) {
+  recipes.get_first_recipe(['salt', 'garlic', 'parsley'], function(err, recipe) {
     if (err) {
       if (err.hasOwnProperty('lastrunstatus')) {
         console.log('ERROR: lastrunstatus: ' + err.lastrunstatus);
@@ -29,41 +29,4 @@
     makeRecipe(recipe)
   });
 
-  // makeRecipe({
-  //   title: 'Antipasto Pasta Salad',
-  //   href: 'http://allrecipes.com/Recipe/Antipasto-Pasta-Salad/Detail.aspx',
-  //   thumbnail: 'http://images.media-allrecipes.com/userphotos/140x140/00/65/56/655688.jpg',
-  //   ingredients: [
-  //    { quantity: '1 pound', name: 'seashell pasta' },
-  //    { quantity: '1/4 pound', name: 'genoa salami' },
-  //    { quantity: '1/4 pound', name: 'pepperoni sausage' },
-  //    { quantity: '1/2 pound', name: 'asiago cheese' },
-  //    { quantity: '1 (6 ounce) can', name: 'black olives' },
-  //    { quantity: '1', name: 'red bell pepper' },
-  //    { quantity: '1', name: 'green bell pepper' },
-  //    { quantity: '3', name: 'tomatoes' },
-  //    { quantity: '1 (.7 ounce) package', name: 'dry italian-style salad dressing mix' },
-  //    { quantity: '3/4 cup', name: 'extra virgin olive oil' },
-  //    { quantity: '1/4 cup', name: 'balsamic vinegar' },
-  //    { quantity: '2 tablespoons', name: 'dried oregano' },
-  //    { quantity: '1 tablespoon', name: 'dried parsley' },
-  //    { quantity: '1 tablespoon', name: 'grated parmesan cheese' },
-  //    { quantity: '', name: 'salt and ground black pepper to taste' } ],
-  // ingredient_names: 
-  //  [ 'seashell pasta',
-  //    'genoa salami',
-  //    'pepperoni sausage',
-  //    'asiago cheese',
-  //    'black olives',
-  //    'red bell pepper',
-  //    'green bell pepper',
-  //    'tomatoes',
-  //    'dry italian-style salad dressing mix',
-  //    'extra virgin olive oil',
-  //    'balsamic vinegar',
-  //    'dried oregano',
-  //    'dried parsley',
-  //    'grated parmesan cheese',
-  //    'salt and ground black pepper to taste' ]
-  // });
 }());
