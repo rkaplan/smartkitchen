8e3291bd04161107c1818ed2ab758ab50fd4b463
Jason Teplitz <jteplitz602@gmail.com>
2014-09-14 02:01:31 -0400

    Added /recipe

---
+++ controllers/Recipes.js
@@ -0,0 +1,34 @@
+(function(){
+  "use strict";
+
+  var base = require("./base.js"),
+      recipes = require("../app/find_recipes.js"),
+
+      RecipesCtrl, _ptype;
+
+  RecipesCtrl = function(schemas){
+    this.schemas = schemas;
+    this.payload = {};
+  };
+
+  _ptype = RecipesCtrl.prototype = base.getProto("api");
+  _ptype._name = "Recipes";
+
+  _ptype.prePrep = function(data, cb){
+    this.schemas.PantryItem.find({present: true}, function(err, items){
+      if (err){ return cb(err) }
+
+      var names = [];
+      for (var i = 0; i < items.length; i++){
+        names.push(items[i].name);
+      }
+      recipes.find_recipe(names, function(err, recipe){
+        if (err){ return cb(err) }
+        console.log(recipe);
+        cb();
+      });
+    });
+  };
+
+  module.exports = RecipesCtrl;
+}());
