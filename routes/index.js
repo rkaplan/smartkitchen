4a2feb59c6408f257eb8b4e6f8fa34410ac34537
Jason Teplitz <jteplitz602@gmail.com>
2014-09-14 07:53:30 -0400

    Added /order_item route

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,16 @@
     Inventory: require("./Inventory.js"),
     Pantry: require("./Pantry.js"),
     Speak: require("./Speak.js"),
-    Recipes: require("./Recipes.js")
+    Recipes: require("./Recipes.js")  ,
+    OrderItem: require("./OrderItem.js")
   };
 
   var routeList = [
     ["/inventory",      Routes.Inventory, 1, 0, 1,  [      "post", "put"]],
     ["/pantry",         Routes.Pantry,    1, 0, 0,  ["get"              ]],
     ["/speak",  Routes.Speak, 1, 0,  ["get" ]],
-    ["/recipes",  Routes.Recipes, 1, 0, 0,  ["get" ]]
+    ["/recipes",  Routes.Recipes, 1, 0, 0,  ["get" ]]  ,
+    ["/order_item",  Routes.OrderItem, 1, 0, 1,  ["post" ]]
   ];
 
   module.exports = routeList;
