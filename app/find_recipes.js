6c8744f94bbc0516dd70483b912cea7fa5e88d24
Jason Teplitz <jteplitz602@gmail.com>
2014-09-14 02:05:14 -0400

    :(

--- app/find_recipes.js
+++ app/find_recipes.js
@@ -3,13 +3,14 @@
 
   var schemas = require('../app/schemas.js'),
       conf = require('nconf').argv().env().file({file: __dirname + '/../config.json'}),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      _ = require("underscore");
 
   //mongoose.connect(conf.get("mongo"));
 
   function find_saved_recipe_with_all_ingredients(ingredients, cb) {
     schemas.Recipe.findOne({ingredient_names : {'$all' : ingredients }}, function(err, doc) {
-      if (err) return cb(err);
+      if (err){ return cb(err); }
       return cb(null, doc);
     });
   }
@@ -28,10 +29,10 @@
   function find_recipe(ingredients, cb) {
     ingredients = _.map(function(ingr) { return ingr.toLowerCase(); });
     find_saved_recipe(ingredients, function(err, recipe) {
-      if (err) return cb(err);
+      if (err){ return cb(err); }
       if (recipe === null) {
         find_new_recipe_by_scraping(ingredients, function(err, recipe) {
-          if (err) return cb(err);
+          if (err){ return cb(err); }
           else {
             console.log('TODO: implement live scraping search as a fallback if our DB has no good results');
           }
@@ -49,6 +50,6 @@
   //   console.log(err);
   //   console.log(result);
   // });
-  exports.find_recipe = find_recipe
+  exports.find_recipe = find_recipe;
 
 }());
